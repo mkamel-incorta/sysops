e59090ce153bde7c0b229be05ca208a63008db46
Mohamed Khaled <mohamed.khaled@incorta.com>
2017-08-30 11:17:09 +0200

    added rootchange script

--- BashScripts/rootchange.sh
+++ BashScripts/rootchange.sh
@@ -1,3 +1,4 @@
 _password=`openssl rand -base64 32` 
 echo -e "$_password\n$_password" | sudo passwd root; echo "$_password"
 echo $_password
+mail -s "[$HOSTNAME Root Password Changed ]" mohamed.khaled@incorta.com <<< "The new root password for $HOSTNAME is $_password "
